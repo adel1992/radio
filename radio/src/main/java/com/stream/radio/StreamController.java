091f1a643920858224dcdbe4bad1ddd2c0a87188
adel1992 <adelmej1992@gmail.com>
2019-05-07 17:34:08 +0100

    changing service path

--- radio/src/main/java/com/stream/radio/StreamController.java
+++ radio/src/main/java/com/stream/radio/StreamController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StreamController {
 
-	@GetMapping("/getStreaming")
+	@GetMapping("/")
 	void getStreaming(HttpServletResponse response) throws MalformedURLException, IOException{
 		response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
 		IOUtils.copy(RadioConnector.getRadioStream("http://radio.mosaiquefm.net:8000/mosalive"), response.getOutputStream());
